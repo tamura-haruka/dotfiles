215a0c51a9bed95db4f869b30e899e99a37bd4fb
Tamura Haruka <haruka@MacBook-Air.local>
2024-08-19 14:31:24 +0900

    <feat> : ⚡ copilotのサジェストの確定のキーマップを変更
    copilotのサジェストの確定のキーマップを<tab>からqqに変更

--- init.lua
+++ init.lua
@@ -571,6 +571,10 @@ require("lazy").setup({
 		{
 			"github/copilot.vim",
 			event = { "InsertEnter"},
+			config = function()
+				vim.g.copilot_no_tab_map = true
+				vim.keymap.set("i", "qq", 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
+    		end
 		},
 		{
 			"CopilotC-Nvim/CopilotChat.nvim",
